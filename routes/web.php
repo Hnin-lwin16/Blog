4ca320808d68284ab320ee39654679f1596fbc2b
Hnin-lwin16 <hninmohmohlwin16@gmail.com>
2025-05-10 20:16:37 +0630

    updateDesign

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,8 @@ Route::get('/', function () {
     });
     return view('blogs',[
         // 'blogs'=> Blog::with('category','author')->get()
-        'blogs'=> Blog::latest()->get()
+        'blogs'=> Blog::latest()->get(),
+        'categories'=>Category::all()
     ]);
 });
 Route::get('/blogs/{blog:slug}', function (Blog $blog)//blog::FindOrFail($slug) 
@@ -33,20 +34,24 @@ Route::get('/blogs/{blog:slug}', function (Blog $blog)//blog::FindOrFail($slug)
     // $blog = Blog::find($slug);
    
     return view("blog",[
-        'blog'=> $blog
+        'blog'=> $blog,
+        'randomBlogs'=>Blog::inRandomOrder()->take(3)->get()
     ]);
 })->where('blog','[A-z\d\-_]+');
 
 Route::get('/categories/{category:slug}',function(Category $category){
     return view('blogs',[
         // 'blogs'=> $category->blogs->load('author','category')
-         'blogs'=> $category->blogs
+         'blogs'=> $category->blogs,
+         'categories'=>Category::all(),
+         'currentCategory'=> $category
     ]);
 });
 
 Route::get('/users/{user:username}',function(User $user){
     return view('blogs',[
         // 'blogs'=>$user->blogs->load('author','category')
-         'blogs'=>$user->blogs
+         'blogs'=>$user->blogs,
+         'categories'=>Category::all()
     ]);
 });
